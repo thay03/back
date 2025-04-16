51508e46ab33c5923d6b277e4f973cb82b798116
thay03 <thayzalida124@gmail.com>
2025-04-16 12:16:55 +0000

    alt

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { createGame,
-     getGames, 
-     updateGame,
-      deleteGame, getGameById } = require('../controllers/gameController');
+      getGames, 
+      updateGame,
+      deleteGame,
+       getGameById } = require('../controllers/gameController');
 
-// Rotas de plantações
+// Rotas de jogos
 router.post('/', createGame);
 router.get('/', getGames);
 router.get('/:id', getGameById);
-router.put('/:id', updateGame); // Rota para atualizar plantação
-router.delete('/:id', deleteGame); // Rota para deletar plantação
+router.put('/:id', updateGame); // Rota para atualizar jogos
+router.delete('/:id', deleteGame); // Rota para deletar jogos
 
 module.exports = router;
